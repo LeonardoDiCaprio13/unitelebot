a77488baf1065ea88f8f07901696fd5bc38c283d
LeonardoDiCaprio13 <42496084+LeonardoDiCaprio13@users.noreply.github.com>
2019-08-08 11:40:51 +0530

    Updated

--- stdplugins/AutomaticBioupdate.py
+++ stdplugins/AutomaticBioupdate.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} |@Detectivechat_bot😁| ⌚️ {HM}"
+        bio = f" {DMY} |@Detectivechat_bot😁| ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
